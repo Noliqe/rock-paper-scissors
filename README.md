5756edbeab1ccf6a639cffab51ad8cff1d799377
Noliqe <tournoliqe1@gmail.com>
2022-07-18 15:38:13 +0200

    Working rock-paper-scissors game

--- README.md
+++ README.md
@@ -2,7 +2,20 @@
 
 This project contains a rock-paper-scissors game!
 
-The project is without html/css at the moment and works only with the browser prompt.
+> Project works now with html/css and javascript using buttons instead of browser prompt.
 
+
+What I've learned by doing this project:
+
+- Learned how to connect html class/id to javascript. (DOM methods)
+- How to change text input using javascript.
+- How to use style using javascript.
+- Learned that if something is wrong, It's the code.
+
+
+Favicon and button icons by icons8.com
+
+Background image: Photo by <a href="https://unsplash.com/@risto_kokkonen?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Risto Kokkonen</a> on <a href="https://unsplash.com/s/photos/abstract-background?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
 
 #Project of "TheOdinProject"
--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-//#Function computerPlay that randomize "Rock, Paper, Scissors" and check with console//
 
+//Computer draws random string from myArray
 function computerPlay(){
     return myArray[Math.floor(Math.random()*myArray.length)];
 }
@@ -7,72 +7,230 @@ function computerPlay(){
 let myArray = ['rock', 'paper', 'scissors'];
 computerPlay(myArray);
 
-//alert(computerPlay())//
 
-//#Function that plays a single round with two parameters, "playerSelection and computerSelection" with a return string for winner/loser//
-
-
-function playRound(playerSelection, computerSelection) {
-     playerSelection = playerSelection.toLowerCase();
-     if (playerSelection == 'rock' && computerSelection == 'rock') {
-        return('draw');
-     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return('lose');
-     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return('win');
-     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return('win');
-     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        return('lose');
-     } else if (playerSelection == 'paper' && computerSelection == 'paper') {
-        return('draw');
-     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return('lose');
-     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return('win');
-     } else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
-        return('draw');
-     }
+//Returns game text, return win or lose to result array and adds computerButtons effect
+function playGame(playerSelection, computerSelection) {
+   if (playerSelection == 'rock' && computerSelection == 'rock') {
+      gameOutput.textContent = "It\'s a tie!\r\n";
+      gameOutput.textContent += "Rock ties with rock";
+      computerOne.setAttribute('style', 'background-color: transparent; box-shadow:  0 0 5px #f9faf8, 0 0 15px #f9faf8, 0 0 30px #f9faf8;');
+      removeRockAnimation();
+      return;
+   } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+      gameOutput.textContent = "You lost!\r\n";
+      gameOutput.textContent += "Rock is beaten by paper";
+      computerTwo.setAttribute('style', 'background-color: transparent; box-shadow:  0 0 5px #f9faf8, 0 0 15px #f9faf8, 0 0 30px #f9faf8;');
+      removePaperAnimation();
+      return('lose');
+   } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+      gameOutput.textContent = "You won\r\n";
+      gameOutput.textContent += "Rock beats scissors";
+      computerThree.setAttribute('style', 'background-color: transparent; box-shadow:  0 0 5px #f9faf8, 0 0 15px #f9faf8, 0 0 30px #f9faf8;');
+      removeScissorsAnimation();
+      return('win');
+   } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+      gameOutput.textContent = "You won\r\n";
+      gameOutput.textContent += "Paper beats rock";
+      computerOne.setAttribute('style', 'background-color: transparent; box-shadow:  0 0 5px #f9faf8, 0 0 15px #f9faf8, 0 0 30px #f9faf8;');
+      removeRockAnimation();
+      return('win');
+   } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+      gameOutput.textContent = "You lost!\r\n";
+      gameOutput.textContent += "Paper is beaten by scissors";
+      computerThree.setAttribute('style', 'background-color: transparent; box-shadow:  0 0 5px #f9faf8, 0 0 15px #f9faf8, 0 0 30px #f9faf8;');
+      removeScissorsAnimation();
+      return('lose');
+   } else if (playerSelection == 'paper' && computerSelection == 'paper') {
+      gameOutput.textContent = "It\'s a tie!\r\n";
+      gameOutput.textContent += "Paper ties with paper";
+      computerTwo.setAttribute('style', 'background-color: transparent; box-shadow:  0 0 5px #f9faf8, 0 0 15px #f9faf8, 0 0 30px #f9faf8;');
+      removePaperAnimation();
+      return;
+   } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+      gameOutput.textContent = "You lost!\r\n";
+      gameOutput.textContent += "Scissors is beaten by rock";
+      computerOne.setAttribute('style', 'background-color: transparent; box-shadow:  0 0 5px #f9faf8, 0 0 15px #f9faf8, 0 0 30px #f9faf8;');
+      removeRockAnimation();
+      return('lose');
+   } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+      gameOutput.textContent = "You won\r\n";
+      gameOutput.textContent += "Scissors beats paper";
+      computerTwo.setAttribute('style', 'background-color: transparent; box-shadow:  0 0 5px #f9faf8, 0 0 15px #f9faf8, 0 0 30px #f9faf8;');
+      removePaperAnimation();
+      return('win');
+   } else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
+      gameOutput.textContent = "It\'s a tie!\r\n";
+      gameOutput.textContent += "Scissors ties with scissors";
+      computerThree.setAttribute('style', 'background-color: transparent; box-shadow:  0 0 5px #f9faf8, 0 0 15px #f9faf8, 0 0 30px #f9faf8;');
+      removeScissorsAnimation();
+      return;
+   }
 }
 
-const playerSelection = prompt("let's play a game! Rock, scissors or paper");
+
 const computerSelection = computerPlay();
 
-//#Function for game within playRound function and play 5 rounds that keep score and reports winner/loser //
+//Person buttons
+const buttonOne = document.querySelector('#btnOne');
+const buttonTwo = document.querySelector('#btnTwo');
+const buttonThree = document.querySelector('#btnThree');
 
-let result = []
-function game() {
-    for (let i = 0; i < 5; i++) {
-        result.push(playRound(playerSelection, computerPlay()));
-    }
-    return result
+buttonOne.setAttribute('style', 'background-color: transparent;');
+buttonTwo.setAttribute('style', 'background-color: transparent;');
+buttonThree.setAttribute('style', 'background-color: transparent;');
+
+//Computer buttons
+const computerOne = document.querySelector('#compOne');
+const computerTwo = document.querySelector('#compTwo');
+const computerThree = document.querySelector('#compThree');
+
+computerOne.setAttribute('style', 'background-color: transparent;');
+computerTwo.setAttribute('style', 'background-color: transparent;');
+computerThree.setAttribute('style', 'background-color: transparent;');
+
+//Reset button
+const resetButton = document.querySelector('#resetBtn');
+resetButton.setAttribute('style', 'display: none;');
+
+//Reset button click event
+resetButton.addEventListener('click', () => {
+   gameOutput.textContent = "Choose Rock Paper Scissors \r\n";
+   gameOutput.textContent += "First to score 5 points wins the game";
+   (inputWon.textContent) = 0;
+   (inputLost.textContent) = 0;
+   result = [];
+   resetButton.setAttribute('style', 'display: none;');
+   return;
+})
+
+//Person buttons click event
+buttonOne.addEventListener('click', () => {
+   if ((inputWon.textContent) == 5 || (inputLost.textContent) == 5) {
+      buttonOne = false;
+      return;
+   } else {
+   let playerSelection = 'rock';
+   result.push(playGame(playerSelection, computerPlay()));
+   return(sendResult());
+   }
+});
+buttonTwo.addEventListener('click', () => {
+   if ((inputWon.textContent) == 5 || (inputLost.textContent) == 5) {
+      buttonOne = false;
+      return;
+   } else {
+    let playerSelection = 'paper';
+    result.push(playGame(playerSelection, computerPlay()));
+    return(sendResult());
+   }
+});
+buttonThree.addEventListener('click', () => {
+   if ((inputWon.textContent) == 5 || (inputLost.textContent) == 5) {
+      buttonOne = false;
+      return;
+   } else {
+    let playerSelection = 'scissors';
+    result.push(playGame(playerSelection, computerPlay()));
+    return(sendResult());
+   }
+});
+
+//Score text
+const paraWon = document.querySelector('#pWon');
+const paraLose = document.querySelector('#pLost');
+
+paraWon.textContent = 'Score:';
+paraLose.textContent = 'Score:';
+
+//Score number
+const inputWon = document.querySelector('#inptWon');
+const inputLost = document.querySelector('#inptLost');
+
+
+
+//Array where win or lose is stored in
+let result = [];
+
+
+//changes score number and reset result array
+function sendResult() {
+     if (result == 'lose' && (inputLost.textContent == 4)) {
+        (inputLost.textContent) = 5;
+        result = [];
+        gameOutput.textContent = "You\'ve lost against the computer! \r\n";
+         gameOutput.textContent += "Try again!";
+        resetButton.setAttribute('style', 'display: flex;');
+        return;
+        } else if (result == 'lose' && (inputLost.textContent == 3)) {
+                (inputLost.textContent) = 4;
+                result = [];
+                return;
+            } else if (result == 'lose' && (inputLost.textContent == 2)) {
+                    (inputLost.textContent) = 3;
+                    result = [];
+                    return;
+                } else if (result == 'lose' && (inputLost.textContent == 1)) {
+                        (inputLost.textContent) = 2;
+                        result = [];
+                        return;
+                    } else if (result == 'lose' && (inputLost.textContent == 0)){
+                        (inputLost.textContent) = 1;
+                        result = [];
+                        return;
+    } else if (result == 'win' && (inputWon.textContent == 4)) {
+            (inputWon.textContent) = 5;
+            result = [];
+            gameOutput.textContent = "You\'ve won against the computer! \r\n";
+            gameOutput.textContent += "Good job!";
+            resetButton.setAttribute('style', 'display: flex;');
+            return;
+        } else if (result == 'win' && (inputWon.textContent == 3)) {
+                (inputWon.textContent) = 4;
+                result = [];
+                return;
+            } else if (result == 'win' && (inputWon.textContent == 2)) {
+                    (inputWon.textContent) = 3;
+                    result = [];
+                    return;
+                } else if (result == 'win' && (inputWon.textContent == 1)) {
+                        (inputWon.textContent) = 2;
+                        result = [];
+                        return;
+                    } else if (result == 'win' && (inputWon.textContent == 0)){
+                            (inputWon.textContent) = 1;
+                            result = [];
+                            return;
+                    } else if (result == 'draw'){
+                              result = [];
+                              return;
+                    } else {
+                     result = [];
+                     return;
+                    }
+}   
+
+//Score before game begins
+inputWon.textContent = 0;
+inputLost.textContent = 0;
+
+//Game display text
+const gameOutput = document.querySelector('#textOutput');
+gameOutput.setAttribute('style', 'white-space: pre; text-align: center;');
+gameOutput.textContent = "Choose Rock Paper Scissors \r\n";
+gameOutput.textContent += "First to score 5 points wins the game";
+
+//Function remove computer effect
+function removeRockAnimation() {
+   setTimeout(() => {computerOne.setAttribute('style', 'background-color: transparent;'); }, 800);
+   return;
 }
-alert(game());
-let numOfWin = result.filter(x => x === "win").length;
-let numOfDraw = result.filter(x => x === "draw").length;
-let numOfLose = result.filter(x => x === "lose").length;
 
-//#Function for winner/loser report
-
-function resultTotal(result){
-    if (numOfWin >= 3) {
-        return('You won!');
-    } else if (numOfWin == 2 && numOfDraw >= 2){
-        return('You won!');
-    } else if (numOfWin == 1 && numOfDraw == 4){
-        return('You won!');
-    } else if (numOfDraw == 5){
-        return('You tied!');
-    } else if (numOfWin == 1 && numOfDraw == 3){
-        return('You tied!');
-    } else if (numOfWin == 2 && numOfDraw == 1){
-        return('You tied!');
-    } else {
-        return('You lost!');
-    }
+function removePaperAnimation() {
+   setTimeout(() => {computerTwo.setAttribute('style', 'background-color: transparent;'); }, 800);
+   return;
 }
 
-
-alert(resultTotal(result));
-
-
+function removeScissorsAnimation() {
+   setTimeout(() => {computerThree.setAttribute('style', 'background-color: transparent;'); }, 800);
+   return;
+}
